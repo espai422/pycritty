7190326fef669b33a44109ec70b43b3b2533a177
espai422 <andreustreaming422@gmail.com>
2021-04-25 03:13:31 +0200

    second commit

--- pruebas.py
+++ pruebas.py
@@ -1,20 +1,20 @@
 import yaml
 
-
+def Themes(data , theme):
 #obrir alavritty.yaml i transformar a diccionari
-with open ('alacritty.yml','r') as f:
-   config = yaml.load(f , Loader=yaml.FullLoader)
+    with open ('alacritty.yml','r') as f:
+    config = yaml.load(f , Loader=yaml.FullLoader)
 
 #obrir tema i transformar en diccionari
-with open ('themes/onedark.yaml','r') as f:
+    with open ('themes/onedark.yaml','r') as f:
     theme = yaml.load(f , Loader=yaml.FullLoader)
 
 #intercambiar alavritty_diccionari.colors --> colors de tema
-config['colors']=theme['colors']
+    config['colors']=theme['colors']
 
-new_config = yaml.dump(config, sort_keys= True)
+    new_config = yaml.dump(config, sort_keys= True)
 #tornar a escriure alacritty.yaml
 
-with open ('alacritty.yml','w') as f:
+    with open ('alacritty.yml','w') as f:
     f.write(new_config)
 
